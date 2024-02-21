fb17a8564c4c550d50236d0f5855168152f706ee
Richard <programmingislife23@gmail.com>
2024-02-21 18:23:32 -0300

    criando o html introduzindo as opcoes

---
+++ app.js
@@ -0,0 +1,8 @@
+/* criar uma funcao para perguntar o que o usuario quer fazer */
+
+function oqueFazer() {
+  const passo = prompt(
+    "O que você quer fazer? Escolhe uma das opções descritas na página."
+  );
+}
+/* criar uma funcao para adicionar um item na lista de tarefas */
