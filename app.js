474b99d8b7f8652961f9359ce53cd1f41cd2a3c4
Richard <programmingislife23@gmail.com>
2024-02-21 18:54:15 -0300

    criado uma lista de tarefas em javascript

--- app.js
+++ app.js
@@ -4,5 +4,35 @@ function oqueFazer() {
   const passo = prompt(
     "O que você quer fazer? Escolhe uma das opções descritas na página."
   );
+  return passo;
 }
 /* criar uma funcao para adicionar um item na lista de tarefas */
+function processoLista(opcaoEscolhida) {
+  if (opcaoEscolhida == "new") {
+    const todo = prompt("Qual tarefa você quer adicionar? ");
+    todoList.push(todo);
+  } else if (opcaoEscolhida == "list") {
+    for (item of todoList) {
+      console.log(`${todoList.indexOf(item)}: ${item}.`);
+    }
+  } else if (opcaoEscolhida == "delete") {
+    const escolherItem = parseInt(
+      prompt("Qual o index do número que você quer deletar? ")
+    );
+    for (item of todoList) {
+      if (escolherItem == todoList.indexOf(item)) {
+        todoList.splice(todoList.indexOf(item), 1);
+      }
+    }
+  } else {
+    continuarRodando = false;
+  }
+}
+
+const todoList = [];
+let continuarRodando = true;
+
+while (continuarRodando == true) {
+  let opcao = oqueFazer();
+  processoLista(opcao);
+}
